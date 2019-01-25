9cf439b3e92b3bc999ad8e843d1713a7d324a9e7
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-25 13:55:03 -0600

    :clipboard: its posible update ticjets

--- apps/tickets/urls.py
+++ apps/tickets/urls.py
@@ -5,5 +5,7 @@ app_name = 'tickets'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('<int:id>/details', views.details, name = 'details'),
+    path('<int:id>/update', views.update, name='update')
     
 ]
--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Ticket
+from django.urls import reverse
+from .models import Ticket, Status
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 def index(request):
     tickets = Ticket.objects.all()
@@ -7,3 +10,34 @@ def index(request):
         'tickets': tickets
     }
     return render(request, "tickets/index.html",context)
+def details(request, id):
+    ticket = get_object_or_404(Ticket, pk = id)
+    users = User.objects.all()
+    statuses = Status.objects.all()
+    context = {
+        'ticket': ticket,
+        'users': users,
+        'statuses': statuses
+    }
+    return render(request,  "tickets/details.html", context)
+def update(request, id):
+    ticket = get_object_or_404(Ticket, pk=id)
+    #try:
+    ticket.title = request.POST['title']
+    ticket.description = request.POST['description']
+    ticket.client = request.POST['client']
+    ticket.user_id = User.objects.get(pk=request.POST['user_id'])
+    ticket.status_id = Status.objects.get(pk=request.POST['status_id'])
+    ticket.save()
+    return HttpResponseRedirect(reverse('tickets:index'))
+    # except:
+    #     users = User.objects.all()
+    #     statuses = Status.objects.all()
+    #     context = {
+    #         'ticket': ticket,
+    #         'users': users,
+    #         'statuses': statuses,
+    #         'error_message': "Error updating the ticket"
+    #     }
+    #     return render(request,  "tickets/details.html", context)
+        
