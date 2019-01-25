95d41adb66af8b464c575f01b4037f8fa18350c6
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-25 13:05:52 -0600

    :clipboard: tickets index view

--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Ticket
 # Create your views here.
+def index(request):
+    tickets = Ticket.objects.all()
+    context = { 
+        'tickets': tickets
+    }
+    return render(request, "tickets/index.html",context)
--- tickets/urls.py
+++ tickets/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.tickets import urls
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tickets/', include(urls))
 ]
