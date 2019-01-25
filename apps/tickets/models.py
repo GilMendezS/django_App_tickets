29b4876a79571ac91af64a8d748406aafa8e4904
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-25 14:13:38 -0600

    :clipboard: fields modified

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -15,10 +15,11 @@ class Ticket(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
     client = models.CharField(max_length = 100, blank = True, null = False)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    status_id = models.ForeignKey(Status, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     deleted_at = models.DateTimeField(blank=True, null = True)
     def __str__(self):
         return self.title
+    
