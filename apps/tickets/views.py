ef88e9a0fca3bf3f9d5bc254916ea8198813ae86
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-25 14:15:02 -0600

    :bug: setting seletec options

--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from .models import Ticket, Status
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 # Create your views here.
 def index(request):
     tickets = Ticket.objects.all()
@@ -22,22 +22,22 @@ def details(request, id):
     return render(request,  "tickets/details.html", context)
 def update(request, id):
     ticket = get_object_or_404(Ticket, pk=id)
-    #try:
-    ticket.title = request.POST['title']
-    ticket.description = request.POST['description']
-    ticket.client = request.POST['client']
-    ticket.user_id = User.objects.get(pk=request.POST['user_id'])
-    ticket.status_id = Status.objects.get(pk=request.POST['status_id'])
-    ticket.save()
-    return HttpResponseRedirect(reverse('tickets:index'))
-    # except:
-    #     users = User.objects.all()
-    #     statuses = Status.objects.all()
-    #     context = {
-    #         'ticket': ticket,
-    #         'users': users,
-    #         'statuses': statuses,
-    #         'error_message': "Error updating the ticket"
-    #     }
-    #     return render(request,  "tickets/details.html", context)
+    try:
+        ticket.title = request.POST['title']
+        ticket.description = request.POST['description']
+        ticket.client = request.POST['client']
+        ticket.user_id = User.objects.get(pk=request.POST['user_id'])
+        ticket.status_id = Status.objects.get(pk=request.POST['status_id'])
+        ticket.save()
+        return HttpResponseRedirect(reverse('tickets:index'))
+    except:
+        users = User.objects.all()
+        statuses = Status.objects.all()
+        context = {
+            'ticket': ticket,
+            'users': users,
+            'statuses': statuses,
+            'error_message': "Error updating the ticket"
+        }
+        return render(request,  "tickets/details.html", context)
         
