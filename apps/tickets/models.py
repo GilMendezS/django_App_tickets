133cd7d81ce012ae9b3ef956486c23f635d17e49
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-25 12:30:53 -0600

    :clipboard: new models

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -1,20 +1,20 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-class BaseModel(models.Model):
-    deleted_at = models.DateTimeField(nullable = True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Status(BaseModel):
-    status = mdoels.CharField(max_length=100, unique= true)
-    
-
-class Ticket(BaseModel):
+       
+class Status(models.Model):
+    title = models.CharField(max_length=100, unique= True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null = True)
+class Ticket(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
-    client = models.CharField(max_length = 100, nullable = False)
-    user_id = models.ForeignKey(User)
-    status_id = models.ForeignKey(Status)
+    client = models.CharField(max_length = 100, blank = True, null = False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    status_id = models.ForeignKey(Status, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null = True)
